af70249c899e61537f4de1269956e1c70218d3c1
Rahul Ravindran <rahul.r945@gmail.com>
2016-07-01 17:39:03 +0530

    proptypes channged

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 * @Date:   2016-07-01T14:54:20+05:30
 * @Email:  rahul.r945@gmail.com
 * @Last modified by:   root
-* @Last modified time: 2016-07-01T15:16:58+05:30
+* @Last modified time: 2016-07-01T17:38:16+05:30
 */
 'use-strict';
 
-var { requireNativeComponent , PropTypes } = require('react-native');
+var { requireNativeComponent , React } = require('react-native');
 
 var iface={
   name: 'LineChart',
   propTypes:{
-    data : PropTypes.arrayOf(PropTypes.object)
+    data : React.PropTypes.arrayOf(React.PropTypes.object)
   }
 
 };
