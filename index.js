760a9662b7696e56a16535270aba4d999cbdd95d
Rahul Ravindran <rahul.r945@gmail.com>
2016-07-04 13:20:05 +0530

    removed react ddependency

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 * @Date:   2016-07-01T14:54:20+05:30
 * @Email:  rahul.r945@gmail.com
 * @Last modified by:   root
-* @Last modified time: 2016-07-01T17:38:16+05:30
+* @Last modified time: 2016-07-04T13:19:27+05:30
 */
 'use-strict';
 
