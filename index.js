14c63749158a869d118415d02b1f02f644cbfe9b
Rahul Ravindran <rahul.r945@gmail.com>
2016-07-05 12:24:58 +0530

    changes

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 * @Date:   2016-07-01T14:54:20+05:30
 * @Email:  rahul.r945@gmail.com
 * @Last modified by:   root
-* @Last modified time: 2016-07-05T12:14:38+05:30
+* @Last modified time: 2016-07-05T12:24:43+05:30
 */
 'use-strict';
 
-import React, {requireNativeComponent , PropTypes } from 'react';
+import React, {PropTypes } from 'react';
 import requireNativeComponent from 'react-native';
 
 var iface={
