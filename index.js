1f335f3dc4956b1bf381cde2a99a5b21c40ef0e4
Rahul Ravindran <rahul.r945@gmail.com>
2016-07-05 14:57:13 +0530

    code changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 * @Date:   2016-07-01T14:54:20+05:30
 * @Email:  rahul.r945@gmail.com
 * @Last modified by:   root
-* @Last modified time: 2016-07-05T14:50:16+05:30
+* @Last modified time: 2016-07-05T14:57:07+05:30
 */
 'use-strict';
 
@@ -13,6 +13,11 @@ import {requireNativeComponent, View }from 'react-native';
 class LineChartView extends Component{
   constructor(props){
     super(props);
+    this.setNativeProps({data : this.props.data})
+  }
+
+  setNativeProps(nativeProps) {
+    this._root.setNativeProps(nativeProps);
   }
 
   render(){
